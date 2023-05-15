5aa0ecb8be8463d11c07dbe9e961bdf7cb0b07e9
Shahryar Salmaan <120364854+shahryarsalmaan@users.noreply.github.com>
2023-05-15 17:47:11 +0600

    Update anonchat_msg.js
    Updated.

--- GoatBot/anonchat_msg.js
+++ GoatBot/anonchat_msg.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const agentUsername = "@youragentname"; // Set your agent username here
+const agentUsername = "@your_agent_username"; // Set your agent username here
 
 const goatBotAnonChatMSGCommand = {
   config: {
@@ -20,14 +20,64 @@ const goatBotAnonChatMSGCommand = {
     dependencies: ["axios"]
   },
 
-  onStart: async function({ api, event, args }) {
-    const content = args.join(" ");
+  onStart: async function({ api, event, args, threadsData, getLang }) {
+  const uid = event.senderID;
 
+  if (args[0] === "on" || args[0] === "off") {
+    if (args.length === 1) {
+      let settings = await threadsData.get(event.threadID, "settings") || {};
+      if (args[0] === "on") {
+        settings.anonchat = settings.anonchat || {};
+        settings.anonchat[uid] = true;
+        await threadsData.set(event.threadID, settings, "settings");
+        return api.sendMessage("AnonChat is now on", event.threadID);
+      } else if (args[0] === "off") {
+        settings.anonchat = settings.anonchat || {};
+        settings.anonchat[uid] = false;
+        await threadsData.set(event.threadID, settings, "settings");
+        return api.sendMessage("AnonChat is now off", event.threadID);
+      }
+    }
+    return;
+  } else {
+    const content = args.join(" ");
     if (!content) {
       return api.sendMessage("Please provide a message to send.", event.threadID);
     }
 
+    const requestData = {
+      uid: uid,
+      agent_username: agentUsername,
+      message: content
+    };
+
+    try {
+      const response = await axios.post("https://anonchat.xaviabot.repl.co/send_message", requestData);
+      const data = response.data;
+
+      if (data.success) {
+        await api.setMessageReaction("✅", event.messageID, event.threadID, api);
+      } else {
+        return api.sendMessage(`${data.message}`, event.threadID);
+      }
+    } catch (error) {
+      console.error(error);
+      return api.sendMessage("An error occurred while sending the message.", event.threadID);
+    }
+  }
+},
+
+  onChat: async function({ api, event, threadsData, isUserCallCommand, getLang }) {
+    if (isUserCallCommand) return;
+
+    const content = event.body;
+
+    if (!content) return;
+
+    let settings = await threadsData.get(event.threadID, "settings") || {};
     const uid = event.senderID;
+
+    if (!settings.anonchat || !settings.anonchat[uid]) return;
 
     const requestData = {
       uid: uid,
