d942d6587a57ea366a8c8fe611b4213910eec4bd
munem1912 <130903016+munem1912@users.noreply.github.com>
2023-09-19 22:26:31 +0600

    Update app.js
    updated /sendTouser for media files

--- XaviaBot/app.js
+++ XaviaBot/app.js
@@ -45,16 +45,40 @@ function startServer(serverAdminPassword) {
 });
 
 app.post('/sendToUser', async (req, res) => {
-    const { uid, message } = req.body;
-    if (!uid || !message) return res.status(400).send('Bad Request');
+  const { uid, message } = req.body;
+  if (!uid || !message) return res.status(400).send('Bad Request');
 
-    try {
+  const linkRegex = /https:\/\/scontent\.xx\.fbcdn\.net\/[^\s]+/g;
+  const links = message.match(linkRegex);
+
+  try {
+    if (links && links.length > 0) {
+      const link = links[0];
+     
+      const response = await axios.post('https://data.anonchatdb.repl.co/download', {
+        url: link,
+      });
+
+      if (response.status === 200) {
+        const responseData = response.data;
+        const newLink = responseData.imageUrl;
+
+        const stream = await global.getStream(newLink);
+
+       
+        const messageInfo = await global.api.sendMessage({ attachment: stream }, uid);
+        return res.status(200).json({ messageInfo });
+      } else {
+        return res.status(500).send('Failed to fetch new link from external API');
+      }
+    } else {
       const messageInfo = await global.api.sendMessage(message, uid);
       return res.status(200).json({ messageInfo });
-    } catch (error) {
-      console.error(error);
-      return res.status(500).send('Internal Server Error');
     }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Internal Server Error');
+  }
 });
 
     app.get('/getConfig', (req, res) => {
